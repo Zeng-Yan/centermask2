449fd078ffdc43fde033583b5b8b03205e82e629
yan <906596588@qq.com>
2021-12-13 12:43:11 +0800

    adding demos

---
+++ demos/onnx_modifier.py
@@ -0,0 +1,93 @@
+# defining funcs that modify the graph of onnx
+# provide a replacement of torch.nonzero() (2-D) by TopK in onnx,
+# Author: zengyan
+# Final: 21.08.24
+# refer: onnx/docs/PythonAPIOverview.md
+# https://github.com/onnx/onnx/blob/f2daca5e9b9315a2034da61c662d2a7ac28a9488/docs/PythonAPIOverview.md#running-shape-inference-on-an-onnx-model
+
+from onnx import helper, shape_inference, version_converter
+from onnx import AttributeProto, TensorProto, GraphProto
+
+
+def make_NonZeroByTopK(x: str, top: str, y: str) -> (list, list):
+    """
+    create onnx nodes and initializers to replace 2D-NonZero() via TopK() due to the bad support of NonZero() in ATC tools of Huawei
+    """
+    # compute k
+    node_sum = helper.make_node('ReduceSum', [x], ['input_sum'], keepdims=0)  # scala里的reduce
+    node_cast_top = helper.make_node('Cast', [top], ['top_f'], to=1)  # to float
+    node_cast_sum = helper.make_node('Cast', ['input_sum'], ['input_sum_f'], to=1)  # to float
+    node_min = helper.make_node('Min', ['input_sum_f', 'top_f'], ['k_f'])  # min(x.sum(), top)
+    node_cast_k = helper.make_node('Cast', ['k_f'], ['k'], to=7)  # to int64
+
+    # top k of x
+    node_flatten_x = helper.make_node('Flatten', [x], ['input_flatten'], axis=0)  # flatten x
+    node_topk = helper.make_node('TopK', ['input_flatten', 'k'], ['k_values', 'k_idx_'], axis=1)  # top k of x
+    node_cast_flatten = helper.make_node('Cast', ['k_idx_'], ['k_idx'], to=7)  # to int64
+
+    # compute idx
+    constant_1 = helper.make_tensor('c_1', TensorProto.INT64, [1], [1])  # [1]
+    node_shape = helper.make_node('Shape', [x], ['input_shape'])  # x.shape
+    node_shape_col = helper.make_node('Gather', ['input_shape', constant_1.name], ['input_col'], axis=0)  # x.shape[1]
+    node_div = helper.make_node('Div', ['k_idx', 'input_col'], ['row'])  # int64: div(int64, int64)
+    node_mod = helper.make_node('Mod', ['k_idx', 'input_col'], ['col'], fmod=0)
+
+    # stack idx
+    node_unsqueeze_r = helper.make_node('Unsqueeze', ['row'], ['row_'], axes=[-1])
+    node_unsqueeze_c = helper.make_node('Unsqueeze', ['col'], ['col_'], axes=[-1])
+    node_concat = helper.make_node('Concat', ['row_', 'col_'], ['y_'], axis=-1)
+    node_squeez = helper.make_node('Squeeze', ['y_'], [y], axes=[0])
+
+    nodes = [node_sum, node_cast_sum, node_cast_top, node_min, node_cast_k,
+             node_flatten_x, node_topk, node_cast_flatten,
+             node_shape, node_shape_col, node_div, node_mod,
+             node_unsqueeze_r, node_unsqueeze_c, node_concat, node_squeez]
+    init = [constant_1]
+    return nodes, init
+
+
+if __name__ == '__main__':
+    '''
+    testing if the output of NonZeroByTopK() match the output of torch.nonzero() with same input
+    '''
+    import torch
+    import onnx
+    import onnxruntime
+    import numpy as np
+
+    N = 3
+    # create a onnx model
+    # Proto details: https://github.com/onnx/onnx/blob/master/onnx/onnx.proto
+    X = helper.make_tensor_value_info('x', TensorProto.INT64, [N, 2])  # Create one input (ValueInfoProto)
+    TOP = helper.make_tensor_value_info('top', TensorProto.INT64, [1])  # Create one input (ValueInfoProto)
+    C = helper.make_tensor_value_info('c_1', TensorProto.INT64, [1])  # Create one input (ValueInfoProto)
+    Y = helper.make_tensor_value_info('y', TensorProto.INT64, [-1, 2])  # Create one output (ValueInfoProto)
+
+    lst_nodes, lst_init = make_NonZeroByTopK('x', 'top', 'y')  # make nodes and initializer
+    graph_def = helper.make_graph(nodes=lst_nodes, initializer=lst_init, name='NonzeroByTopK',
+                                  inputs=[X, TOP, C], outputs=[Y])  # Create the graph (GraphProto)
+
+    model_def = helper.make_model(graph_def, producer_name='nonzero')  # Create the model (ModelProto)
+    model_def.opset_import[0].version = 11  # specify version
+    model_def.ir_version = 7  # specify version
+
+    onnx.checker.check_model(model_def)  # check model
+    onnx.save(model_def, 'NonzeroByTopK.onnx')  # save model
+    # model_onnx = onnx.load_model('NonzeroByTopK.onnx')
+    # inferred_model = shape_inference.infer_shapes(model_def)
+    # print(inferred_model.graph.value_info)
+
+    # test model
+    input_tensor = torch.randint(0, 2, (N, 2))
+    print(f'\ninput: \n{input_tensor.detach().cpu().numpy()}')
+    output = torch.nonzero(input_tensor)
+    array = output.detach().cpu().numpy()
+    print(f'\nNonzero() of input: \n{array}\nshape: \n{output.shape}')
+    input_tensor = input_tensor.detach().cpu().numpy()
+    limit = torch.tensor([0], dtype=torch.int64).detach().cpu().numpy()
+    onnx_session = onnxruntime.InferenceSession('NonzeroByTopK.onnx')
+    output = onnx_session.run(['y'], {'x': input_tensor, 'top': limit})
+    print(f'\nNonzeroByTopK() of input: \n{output[0]}\n shape: {output[0].shape}\n')
+
+# 坑点1 把常量加入input
+# 坑点2 修改ir_verion 和opset version
---
+++ demos/replace_Nozero.py
@@ -0,0 +1,68 @@
+import torch
+import onnx
+import onnxruntime
+
+
+def nonzero(**kwargs) -> torch.tensor:
+    """
+    由于华为方不支持nozero算子，该函数用topk规避nonzero
+    """
+    if not kwargs['m']:  # 正常执行nonzero算子
+        idx = torch.nonzero(kwargs['input'], )
+    else:  # 导出onnx时执行的分支
+        x = kwargs['input'].to(torch.float32)  # bool/? -> int64 统一数据类型避免奇怪的错误
+        k = torch.sum(x)  # 设置k值
+        if x.ndim == 1:
+            k = torch.min(torch.tensor(x.shape[0]).to(torch.float32), k)  # 一维情况下避免索引越界，op 11 要求min为float
+            k = k.reshape(1).to(torch.int64)  # topk的k必须是1d int64
+            _, idx = x.topk(k.item())
+            idx = idx.unsqueeze(-1)  # [M, 1] 改变形状对应nonzero的输出
+        else:  # 输入为二维情况下的执行分支
+            fixed_dim = torch.tensor(x.shape[1], dtype=torch.int64)  # [80] 记录固定的列数，以便还原二维索引
+            x = x.flatten()  # [N, 80] -> [N*80]  奇怪的地方，这里被onnx换成了reshape
+            nms_top = kwargs['nms_top']  # nms_top仅在二维情况下生效
+            k = torch.min(nms_top.to(torch.float32), k)  # op 11 要求min为float
+            k = k.reshape(1).to(torch.int64)  # topk的k必须是1d int64
+            _, col_idx = x.topk(k.item())  # 将二维tensor展平后用topk规避
+            col_idx = col_idx.to(torch.int64)  # 增加cast便于onnx修改算子
+            row_idx = (col_idx / fixed_dim).floor().to(torch.int64)  # topk在原二维tensor对应的行索引
+            # col_idx = col_idx.fmod(fixed_dim).to(torch.int64)  # topk在原二维tensor对应的列索引
+            col_idx = (col_idx - row_idx * fixed_dim).to(torch.int64)  # opset9 不支持fmod
+            idx = torch.stack((row_idx, col_idx), dim=-1)  # [k, 2] 合并为[[行索引, 列索引], ...]的形式
+        idx = idx[0:k[0]]  # 一个无意义的操作来保留onnx中k的计算
+
+    return idx.to(torch.int64)
+
+
+class TestNet(torch.nn.Module):
+    def __init__(self, m):
+        super(TestNet, self).__init__()
+        self.m = m
+
+    def forward(self, inp):
+        return nonzero(input=inp, as_tuple=False, nms_top=torch.tensor(10), m=self.m)
+
+
+net1 = TestNet(False)
+net2 = TestNet(True)
+x = torch.randint(0, 2, (3, 2))
+print(x)
+x_array = x.detach().cpu().numpy()
+
+onnx_path = 'Net1.onnx'
+torch.onnx.export(net1, x, onnx_path,
+                  input_names=['x'], output_names=['y'], dynamic_axes={'y': [0]},
+                  opset_version=11, verbose=False)
+
+onnx_session = onnxruntime.InferenceSession(onnx_path)
+output = onnx_session.run(['y'], {'x': x_array})
+print(output)
+
+onnx_path = 'Net2.onnx'
+torch.onnx.export(net2, x, onnx_path,
+                  input_names=['x'], output_names=['y'], dynamic_axes={'y': [0]},
+                  opset_version=11, verbose=False)
+
+onnx_session = onnxruntime.InferenceSession(onnx_path)
+output = onnx_session.run(['y'], {'x': x_array})
+print(output)
